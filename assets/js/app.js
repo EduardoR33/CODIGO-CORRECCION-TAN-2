d33b2a5bad1ebf8c833b18dcdfb2bf785e814b3a
Luis Eduardo Varela <EduardoVarela_1996@hotmail.com>
2025-05-26 11:51:06 -0600

    feat: Correcciones de Luis Varela

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('.name'); //name no es valido con las comillas sencillas, necesita el .
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //Se agregoAsync en la función displayUser para que el await funcionara 
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  
+try { //Se implemento un bloque try para ejecutar 
+    const response = await fetch('${usersEndpoint}/${username}');
+    const data = await response.json(); // Corregido: faltaba extraer el JSON
+    console.log(data);
+    $n.textContent = data.name || 'Nombre no disponible';//Se cambio la estructura con un or ||
+    $b.textContent = data.blog || 'Blog no disponible';//Se cambio la estructura con un or ||
+    $l.textContent = data.location || 'Ubicación no disponible'; //Se cambio la estructura con un or ||
+  } catch (err) {//En caso de no ejecutarse el bloque try, el bloque catch se ejecutara
+    handleError(err);
+  }
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log('¡OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = 'Algo salió mal: ${err.message}';
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
